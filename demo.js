9cdcc45b181f22e7fd59d0bacf2d185e0c108123
ADITYADEB072003 <63070122+ADITYADEB072003@users.noreply.github.com>
2020-05-10 17:45:33 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,52 @@
+
+window.onload=function(){
+var head=document.getElementById('h')
+head.style.color="white"
+head.style.backgroundColor="black"
+head.style.transform="uppercase";
+var ved= document.getElementById("para");
+ved.innerHTML="chainsmoker"
+var ved2= document.getElementById("para2");
+ved2.innerHTML="commando3"
+var ved2= document.getElementById("para3");
+ved2.innerHTML="photos"
+var f=document.getElementById('vi')
+f.src="Photo Reprise Version (Hindi_Punjabi) x Zara Tasveer Se Tu _ Luka Chuppi _ Acoustic Singh - YouTube (720p).mp4"
+
+var u=document.getElementById('vi2')
+u.src = "commando3.mp4"
+var u=document.getElementById('vi3')
+u.src = "Dhvani Bhanushali Live _ IIFA Rocks Performance 2019 - YouTube (720p).mp4"
+var u=document.getElementById('vi4')
+u.src = "Travel Medley 2018 _ Twin Strings Ft. Manav, Pavitra - YouTube (720p).mp4"
+var u=document.getElementById('vi5')
+u.src = "Aaj Bhi (Official Video) - Vishal Mishra _ Ali Fazal, Surbhi Jyoti _ VYRLOriginals - YouTube (720p).mp4"
+var u=document.getElementById('vi6')
+u.src = "Arijit Singh with his soulful performance _ 6th Royal Stag Mirchi Music Awards _ Radio Mirchi - YouTube (360p).mp4"
+var u=document.getElementById('vi7')
+u.src = "Arijit Singh _ Yaariyan _ Full Song _ Rajma Chawal Movie _ 2018 - YouTube (360p).mp4"
+var u=document.getElementById('vi8')
+u.src = "Dilbara - Full Song _ Dhoom _ Abhishek Bachchan _ Uday Chopra _ Esha Deol _ Abhijeet _ Sowmya - YouTube (720p).mp4"
+var u=document.getElementById('vi9')
+u.src = "Crazy Kiya Re - Full Song _ Dhoom_2 _ Hrithik Roshan _ Aishwarya Rai _ Sunidhi Chauhan - YouTube (720p).mp4"
+var u=document.getElementById('vi10')
+u.src = "Bandeya Rey Bandeya Lyrical _ SIMMBA _ Ranveer Singh, Sara Ali Khan _ Arijit Singh _ Asees Kaur - YouTube (720p).mp4"
+var u=document.getElementById('vi11')
+u.src = "LYRICAL_ Saugandh Mujhe Iss Mitti Ki Song _ PM Narendra Modi _ Vivek Oberoi_Sukhwinder Singh,Shashi - YouTube (720p).mp4"
+var u=document.getElementById('vi12')
+u.src = "s.mp4"
+var e = document.getElementsByTagName('a');
+e[0].href="Photo Reprise Version (Hindi_Punjabi) x Zara Tasveer Se Tu _ Luka Chuppi _ Acoustic Singh - YouTube (720p).mp4";
+e[2].href="Dhvani Bhanushali Live _ IIFA Rocks Performance 2019 - YouTube (720p).mp4";
+e[1].href="commando3.mp4";
+e[3].href="Travel Medley 2018 _ Twin Strings Ft. Manav, Pavitra - YouTube (720p).mp4"
+e[4].href="Aaj Bhi (Official Video) - Vishal Mishra _ Ali Fazal, Surbhi Jyoti _ VYRLOriginals - YouTube (720p).mp4"
+e[5].href="Arijit Singh with his soulful performance _ 6th Royal Stag Mirchi Music Awards _ Radio Mirchi - YouTube (360p).mp4"
+e[8].href="Crazy Kiya Re - Full Song _ Dhoom_2 _ Hrithik Roshan _ Aishwarya Rai _ Sunidhi Chauhan - YouTube (720p).mp4"
+e[6].href="Arijit Singh _ Yaariyan _ Full Song _ Rajma Chawal Movie _ 2018 - YouTube (360p).mp4"
+e[7].href="Dilbara - Full Song _ Dhoom _ Abhishek Bachchan _ Uday Chopra _ Esha Deol _ Abhijeet _ Sowmya - YouTube (720p).mp4"
+e[9].href="Bandeya Rey Bandeya Lyrical _ SIMMBA _ Ranveer Singh, Sara Ali Khan _ Arijit Singh _ Asees Kaur - YouTube (720p).mp4"
+e[10].href="LYRICAL_ Saugandh Mujhe Iss Mitti Ki Song _ PM Narendra Modi _ Vivek Oberoi_Sukhwinder Singh,Shashi - YouTube (720p).mp4"
+e[].href="s.mp4"
+
+}
